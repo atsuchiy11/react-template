1be212b11993147f35cb428ac1aa2635d3926b22
atsuchiy11 <atsuchiy11@gmail.com>
2022-03-07 17:33:13 +0900

    ts-fix

--- ts/src/index.tsx
+++ ts/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { render } from "react-dom"
-// import HelloWorld from "./components/HelloWorld";
 import HelloWorld from "src/components/HelloWorld"
 
 render(<HelloWorld />, document.getElementById("root"))
